69d13d8351439c2958adc6f0d900917916b3d660
toshimatagi <toshimatagi@gmail.com>
2024-06-15 21:59:42 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+console.log('Before route definition');
+
 app.get('/', (req, res) => {
+    console.log('Inside GET / route');
     res.send('Hello World!');
 });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+console.log('After app.listen');
